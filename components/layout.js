60cfdf28cabf3592df62d541d94c810812f042dd
Hankyu2000 <yuduki.developer@gmail.com>
2023-04-23 15:18:46 +0900

    Fun to play

--- components/layout.js
+++ components/layout.js
@@ -8,28 +8,30 @@ export default function Layout({children}){
                 <title>ピカデリー軍集団</title>
             </Head>
             <header>
-                <div className="navbar bg-base-100">
+                <div className="navbar text-black bg-blue-200">
+
                     <div className="flex-1">
-                        <Link className="btn btn-ghost normal-case text-xl" href="/">ピカデリー軍集団</Link>
+                        <Link className="btn btn-ghost normal-case text-xl" href="/"></Link>
                     </div>
                     <div className="flex-none">
                         <ul className="menu menu-horizontal px-1">
-                            <li><Link href="/about/kirov">モルトーク置き場</Link></li>
+                            <li><Link href="/about/kirov">我々の睡眠</Link></li>
                             <li tabIndex="0">
-                                <a>About</a>
-                                <ul className="p-2 bg-base-100">
+                                <a>指揮官の声</a>
+                                <ul className="p-2 bg-base-300">
                                     <li><Link href="/about/kirov">キーロフについて</Link></li>
                                     <li><Link href="/about/Kairi">カイリについて</Link></li>
                                 </ul>
                             </li>
-                            <li><Link href="/forces">我々の編成</Link></li>
+                            <li><Link href="/forces">部隊編成</Link></li>
+                            <li><Link href="/warfare">装備</Link></li>
                         </ul>
                     </div>
                 </div>
             </header>
-            <div className="m-5">
+            <main className="m-5">
                 {children}
-            </div>
+            </main>
         </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  darkMode: 'media',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    './components/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
